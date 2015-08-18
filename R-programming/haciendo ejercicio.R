6cf4b3c5d52e5e0abeaaf4488046a36fddb226f6
Usuarios R Nicaragua <morales.economia@gmail.com>
2015-08-17 20:44:44 -0600

    Primer ejercicio

--- R-programming/haciendo ejercicio.R
+++ R-programming/haciendo ejercicio.R
@@ -59,14 +59,22 @@ pollutantmean <- function(directory, pollutant, id) {
 }
 
 
-dat <- data.frame()
-for(i in 1:10) {
-  pollutant = read.csv(locarchivo[i])
-  pollutant2 =  merge(pollutant,id)
+dato <- data.frame()
+for(i in id) {
+  dato <- rbind(dato, read.csv(locarchivo[i]))
 }
 
 
-summary(pollutant2, na.rm=TRUE)
+
+
+
+
+
+summary(dato, na.rm=TRUE)
+
+
+
+
 
 
 summary(merge(read.csv(locarchivo[1:2])))
@@ -80,3 +88,46 @@ http://www.tech.theplayhub.com/reading_multiple_files_and_calculating_mean_based
   
 
 
+  pollutants <- function(directory, pollutant, id) {
+    nombre = paste(formatC(id, width=3, flag = "0"), ".csv", sep="")
+    url = "https://raw.githubusercontent.com/usuariosrnicaragua/Coursera-R/master/R-programming/"
+    locarchivo = paste(url, "specdata", "/", nombre, sep = "")
+    leer = read.csv(locarchivo)
+  
+    
+    download.file(locarchivo, destfile=nombre, method='curl')
+
+    
+    http://xmuxiaomo.github.io/2015/06/10/R-Programming-Assignment-1/
+    
+    
+
+  
+  
+      
+      
+############################################Primera tarea###################
+############################################################################
+      
+pollutants <- function(directory, pollutant, id) {
+  nombre = paste(formatC(id, width=3, flag = "0"), ".csv", sep="")
+  locarchivo = paste(directory, "/", nombre, sep = "")
+  dato <- data.frame()
+  for(i in id) {
+    dato <- rbind(dato, read.csv(locarchivo[i]))
+  }
+  mean(dato[[pollutant]], na.rm=TRUE)
+}
+#############################################################################
+#############################################################################
+    
+"~/GitHub/Coursera-R/R-programming/"
+##############################################################################    
+    
+    
+    
+    
+    
+    
+    
+    
